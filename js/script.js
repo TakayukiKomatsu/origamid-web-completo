afe12a6f7661abeb1835629dece788f697e1caf1
Takayuki Komatsu <komatsutakayuki235@live.com>
2020-12-17 13:44:07 -0300

    Add Script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    time: 5000,
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    nav: true,
+    time: 5000,
+    auto: true,
+  });
+}
